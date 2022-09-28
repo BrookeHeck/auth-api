f70074a2d9a223cb6cd4d2cdb2f454deef19ce17
Brooke Heck <b19heck@gmail.com>
2022-09-28 18:08:16 +0000

    add auth routes

--- auth-api-server/src/server.js
+++ auth-api-server/src/server.js
@@ -1,11 +1,13 @@
 'use strict';
 
+const { sign } = require('crypto');
 const express = require('express');
 
 const notFoundHandler = require('./../src/auth/error-handlers/404');
 const errorHandler = require('./../src/auth/error-handlers/500');
 const logger = require('./../src/auth/middleware/logger.js');
 
+const authRoutes = require('./../src/auth/routes/authRouter');
 const v1Routes = require('./../src/auth/routes/v1');
 
 const app = express();
@@ -13,6 +15,8 @@ const app = express();
 app.use(express.json());
 
 app.use(logger);
+
+app.use(authRoutes);
 
 app.use('/api/v1', v1Routes);
 
