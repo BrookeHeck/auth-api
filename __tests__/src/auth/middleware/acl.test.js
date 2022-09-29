2397bea6b4a6f56423fb108f0cbd17e1297cc626
Brooke Heck <b19heck@gmail.com>
2022-09-29 16:56:40 +0000

    add test files

--- __tests__/src/auth/middleware/acl.test.js
+++ __tests__/src/auth/middleware/acl.test.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const { exportAllDeclaration } = require("@babel/types");
-
-
 describe('test', () => {
   test('test', () => {
     expect(true).toBeTruthy();
---
+++ __tests__/src/auth/routes/signin.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('test', () => {
+  test('test', () => {
+    expect(true).toBeTruthy();
+  });
+});
